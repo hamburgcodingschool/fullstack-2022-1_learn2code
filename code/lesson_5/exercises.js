1b028316b245c78cec9fb6215efa292ba84f4561
Paul Anton <paul.antton@gmail.com>
2022-03-02 21:12:17 +0100

    Lesson 8 fix

---
+++ code/lesson_5/exercises.js
@@ -0,0 +1,31 @@
+// E0. - E3 from previous class
+
+// Start with 2 hardcoded arrays, e.g. prices1 & prices2
+// Calculate a third array that contains the sum of the elements in the first two arrays.
+
+prices1 = [12, 16, 24, 28] 
+prices2 = [1, 1, 1, 1]
+pricesSum = []
+for (i=0; i < prices1.length; i++){
+
+}
+
+console.log("Result: " + pricesSum);
+// expected result: [13, 17, 25, 29] 
+
+
+// E1. Increase all prices larger than 2000 with 10%; 
+prices = [120, 1600, 2400, 2800] 
+for (i=0; i < prices.length; i++){
+
+}
+console.log("Result: " + prices);
+
+// if - else // if else if 
+
+// E2.
+// For each number in an array, print the following:
+// "Negative", if the number is negative;
+// "Positive", if the number is positive;
+// "Zero", if the number is positive;
+
